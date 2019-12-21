99d030c8393ca91869f774a370eb185d9c3a7479
anstjaos <vv_eros@naver.com>
2019-12-21 14:04:43 +0900

    [#2] DB 내용 페이지 렌더링
    - 디비에서 불러온 값들 페이지 렌더링
    
    - mybatis 테스트용 파일들 삭제
    
    Resolves #2

--- src/main/java/com/develop/guestbook/Mapper/PopItMapper.java
+++
@@ -1,7 +0,0 @@
-package com.develop.guestbook.Mapper;
-import java.util.List;
-import com.develop.guestbook.DAO.PopItDAO;
-
-public interface PopItMapper {
-	public List<PopItDAO> selectList();
-}
--- src/main/java/com/develop/guestbook/Service/PopItService.java
+++
@@ -1,7 +0,0 @@
-package com.develop.guestbook.Service;
-import java.util.List;
-import com.develop.guestbook.DAO.PopItDAO;
-
-public interface PopItService {
-	public List<PopItDAO> selectListService();
-}
