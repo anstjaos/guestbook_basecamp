5b57ffe3eb39d24364b5ff1783f80ae98db06f5a
anstjaos <vv_eros@naver.com>
2020-01-05 20:00:53 +0900

    Update files to JUnit5

--- src/test/java/com/develop/guestbook/Controller/WriteGuestbookControllerTest.java
+++ src/test/java/com/develop/guestbook/Controller/WriteGuestbookControllerTest.java
@@ -6,36 +6,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.develop.guestbook.Service.GuestbookService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml", "file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-@WebAppConfiguration
+@WebAppConfiguration()
 public class WriteGuestbookControllerTest {
-	@Mock
-    GuestbookService guestbookService;
-	@InjectMocks
-    private WriteGuestbookController writeGuestbookController;
-	
 	@Autowired
 	private WebApplicationContext wac;
 	
 	private MockMvc mockMvc;
 	
-	@Before
+	@BeforeEach
 	public void setup() throws Exception {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
